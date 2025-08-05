6b221b2763fb7fdf900437dfe7fc0c3c20fe145d
MarcosBarradas <vini.moraes.1518@email.com>
2025-08-05 15:25:05 -0300

    - Implementar função que permite o cliente inserir nomes no campo de texto   - Salvar nome no array   - Validar se campo está vazio ou se nome já foi inserido   - Limpar o campo após inserção

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-let amigo = [];
+let amigos = [];
 
+let buttonAdd = document.querySelector('.button-add'); //utilizei addeventlistener pois é uma prática mais recomendada
+buttonAdd.addEventListener('click', adicionarAmigo); //em comparação com o onclick
+
+function adicionarAmigo(e){
+    e.preventDefault();
+
+    let campoDeTexto = document.querySelector('#amigo');
+    let nome = campoDeTexto.value.trim(); //trim para tirar espaço em branco                            
+
+    if(nome === ''){
+        alert("Por favor, insira um nome.");
+    } else if (amigos.includes(nome)) {
+        alert("Nome já existente na lista!");
+    } else {
+        amigos.push(nome);
+    }
+    
+    campoDeTexto.value = "";
+    console.log(amigos)
+
+}
